8f507cbf6752cb4f70a6764ce4b3582bd41da450
ULXQC1EK2YS96HN\Administrator <zhuxuyong@ucforward.com>
2019-01-23 15:20:41 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+// import logo from './logo.svg';
 import './App.css';
+import './less/login.css'
+import Home from './pages/home/home.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
+        <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <ul>
+              <li>
+                <Link to="/home">渣渣</Link>
+              </li>
+            </ul>
         </header>
+        <div className="login">
+           <Route path="/home" component={Home}/>
+        </div>
       </div>
-    );
+      </Router>
+      
+    )
   }
 }
 
---
+++ src/pages/home/home.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import '../../less/home.css'
+
+class Home extends Component {
+    render(){
+        return (
+            <div className="home">
+                <ul>
+                    <li>
+                        <span>fsdf</span>
+                        <span>gdfgf</span>
+                    </li>
+                    <li>
+                        <span>fsdf</span>
+                        <span>gdfgf</span>
+                    </li>
+                    <li>
+                        <span>fsdf</span>
+                        <span>gdfgf</span>
+                    </li>
+                    <li>
+                        <span>fsdf</span>
+                        <span>gdfgf</span>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Home
